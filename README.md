4a67426bb2f9c4a2f65195d4c956fc0d3c432cad
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2023-12-15 15:29:19 -0300

    fix: integration security in routes, like validacions and some return msg diferent

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# 20232BSET03P2
+# 20232BSET03P2 - Abner
 Inteli - Engenharia de Software | Avaliação 2023-2B P2
+
+## Vulnerabilidades encontradas
+
+### Vulnerabilidade 1 (Construção da tabela)
+
+No momento que é criado a tabela de dogs e cats o id é apenas um valor int sem autoincrement, além de a tabela não identificar como um id, gerando problemas futuros como inserção direta do id, além de dificultar quando for realizar um join nas tabelas caso o sistema venha evoluir
+
+<strong>Solução</strong>
+
+Modifiquei a forma como ta sendo criado as tabelas.
+
+```js
+db.serialize(() => {
+  db.run("CREATE TABLE cats (id INTEGER PRIMARY KEY, name TEXT, votes INT)");
+  db.run("CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, votes INT)");
+});
+```
+
+### Vulnerabilidade 2 (Validação do name nas rotas de inserção)
+
+As rotas não possuem nenhuma validação de sql injection, valor presente ou até mesmo o tamanho dele, gerando assim N brechas de fazer a aplicação cair ou para de funcionar.
+
+
+<strong>Solução</strong>
+
+Implementei validações nas rotas para prevenir inserções erradas ou que estejam com algum problema
+
+```js
+  // Validação do input em si, se vem com algum valor presente
+  if(name.length === 0 || name.length < 2){
+    return res.status(400).json({ message: "Nome inválido !!" });
+  }
+
+  // Implementado o REGEX para validar < > = e ' ' no campo nome
+  const nameRegex = /[<>= ]/;
+
+  if (nameRegex.test(name)) {
+    return res.status(400).json({ message: "Nome inválido !!" });
+  }
+
+
+  // Verificação de existência de dados registrado
+  if(catsNameArray.includes(name)){
+    return res.status(400).json({ message: "Nome de gato já registrado !" });
+  }
+});
+```
+
+### Vulnerabilidade 3 (Validação da rota de adicionar voto)
+
+As rotas não possuem nenhuma validação de sql injection, valor presente ou até mesmo o tamanho dele, gerando assim N brechas de fazer a aplicação cair ou para de funcionar. Nem mesmo possuía validação de valores presentes, podendo acontecer vários problemas futuros ou até mesmo troca nos valores de voto
+
+<strong>Solução</strong>
+
+Implementei validações nas rotas para prevenir inserções erradas ou que estejam com algum problema, além de verificar se o id do animal realmente existe para realizar voto, retornando um erro caso não exista
+
+```js
+
+ const animalType = req.params.animalType;
+  const id = parseInt(req.params.id);
+
+    // Validando se os valores realmente estão dentro do esperado de receber
+  if(animalType !== 'cats' | 'dogs'){
+    return res.status(400).json({ message: "Precisa passar um valor valido do tipo de animal de preferência ( cats ou dogs )" });
+  }
+
+    // Verificando se o ID é realmente um valor numero para prevenir erros de inserção
+  if (typeof id !== "number") {
+    return res.status(400).json({message:"Valor do id invalido"});
+  }
+
+
+    // Verifico se realmente existe esse valor no banco caso não, ele retornara um erro para o usuário
+  db.get(`SELECT * FROM ${animalType} WHERE id = ${id}`, (err, rows) => {
+    if (err) {
+      return res.status(500).json({message:"Erro ao consultar o banco de dados"});
+  } else if (!rows) {
+      return res.status(404).json({message:"Houve um problema ao registar o voto, tente mais tarde !"});
+  }
+  })
+```
+
--- index.js
+++ index.js
@@ -9,15 +9,40 @@ app.use(bodyParser.json());
 
 
 const db = new sqlite3.Database(':memory:');
+const catsNameArray = []
+const dogsNameArray = []
 
+// Implementado o autoincrement no banco para assim gerar um id automático para cada novo dado
 db.serialize(() => {
-  db.run("CREATE TABLE cats (id INT, name TEXT, votes INT)");
-  db.run("CREATE TABLE dogs (id INT, name TEXT, votes INT)");
+  db.run("CREATE TABLE cats (id INTEGER PRIMARY KEY, name TEXT, votes INT)");
+  db.run("CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, votes INT)");
 });
 
 app.post('/cats', (req, res) => {
   const name = req.body.name;
-  db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
+
+  // Validação do input em si, se vem com algum valor presente
+  if(name.length === 0 || name.length < 2){
+    return res.status(400).json({ message: "Nome inválido !!" });
+  }
+
+  // Implementado o REGEX para validar < > = e ' ' no campo nome
+  const nameRegex = /[<>= ]/;
+
+  if (nameRegex.test(name)) {
+    return res.status(400).json({ message: "Nome inválido !!" });
+  }
+
+
+  // Verificação de existência de dados registrado
+  if(catsNameArray.includes(name)){
+    return res.status(400).json({ message: "Nome de gato já registrado !" });
+  }
+
+  catsNameArray.push(name)
+
+  // Mudando a forma como é utilizada a função para assim ela garantir que vai inserir um valor em formato string e não algo dentro do código
+  db.run(`INSERT INTO cats (name, votes) VALUES (?,0)`,[name], function(err) {
     if (err) {
       res.status(500).send("Erro ao inserir no banco de dados");
     } else {
@@ -27,13 +52,66 @@ app.post('/cats', (req, res) => {
 });
 
 app.post('/dogs', (req, res) => {
+
+  const name = req.body.name;
+
+  // Validação do input em si, se vem com algum valor presente
+  if(name.length === 0 || name.length < 2){
+    return res.status(400).json({ message: "Nome inválido !!" });
+  }
+
+  // Implementado o REGEX para validar < > = e ' ' no campo nome
+  const nameRegex = /[<>= ]/;
+
+  if (nameRegex.test(name)) {
+    return res.status(400).json({ message: "Nome inválido !!" });
+  }
+
+
+  // Verificação de existência de dados registrado
+  if(dogsNameArray.includes(name)){
+    return res.status(400).json({ message: "Nome de cachorro já registrado !" });
+  }
+
+  dogsNameArray.push(name)
+
+  // Mudando a forma como é utilizada a função para assim ela garantir que vai inserir um valor em formato string e não algo dentro do código
+  db.run(`INSERT INTO dogs (name, votes) VALUES (?,0)`,[name], function(err) {
+    if (err) {
+      res.status(500).send("Erro ao inserir no banco de dados");
+    } else {
+      res.status(201).json({ id: this.lastID, name, votes: 0 });
+    }
+  });
   
 });
 
-app.post('/vote/:animalType/:id', (req, res) => {
+app.post('/vote/:animalType/:id', async (req, res) => {
+
+  const animalType = req.params.animalType;
+  const id = parseInt(req.params.id);
+
+  if(animalType !== 'cats' | 'dogs'){
+    return res.status(400).json({ message: "Precisa passar um valor valido do tipo de animal de preferência ( cats ou dogs )" });
+  }
+
+  if (typeof id !== "number") {
+    return res.status(400).json({message:"Valor do id invalido"});
+  }
+
+  console.log(id)
+
+  db.get(`SELECT * FROM ${animalType} WHERE id = ${id}`, (err, rows) => {
+    if (err) {
+      return res.status(500).json({message:"Erro ao consultar o banco de dados"});
+  } else if (!rows) {
+      return res.status(404).json({message:"Houve um problema ao registar o voto, tente mais tarde !"});
+  }
+  })
+
  
   db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
-  res.status(200).send("Voto computado");
+  return res.status(200).send("Voto computado");
 });
 
 app.get('/cats', (req, res) => {
@@ -47,7 +125,13 @@ app.get('/cats', (req, res) => {
 });
 
 app.get('/dogs', (req, res) => {
-  
+  db.all("SELECT * FROM cats", [], (err, rows) => {
+    if (err) {
+      res.status(500).send("Erro ao consultar o banco de dados");
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 app.use((err, req, res, next) => {
